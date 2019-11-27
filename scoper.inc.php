50d4da5836e910741d48d961a299c43211e1fede
Iain <iain@polevaultweb.com>
2019-11-27 16:56:22 +0000

    Update scoper file.

--- scoper.inc.php
+++ scoper.inc.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 use Isolated\Symfony\Component\Finder\Finder;
 
 return [
+    // The prefix configuration. If a non null value will be used, a random prefix will be generated.
+    'prefix' => null,
+
     // By default when running php-scoper add-prefix, it will prefix all relevant code found in the current working
     // directory. You can however define which files should be scoped by defining a collection of Finders in the
     // following configuration key.
@@ -30,6 +33,13 @@ return [
         ]),
     ],
 
+    // Whitelists a list of files. Unlike the other whitelist related features, this one is about completely leaving
+    // a file untouched.
+    // Paths are relative to the configuration file unless if they are already absolute
+    'files-whitelist' => [
+        'src/a-whitelisted-file.php',
+    ],
+
     // When scoping PHP files, there will be scenarios where some of the code being scoped indirectly references the
     // original namespace. These will include, for example, strings or string manipulations. PHP-Scoper has limited
     // support for prefixing such strings. To circumvent that, you can define patchers to manipulate the file to your
@@ -37,24 +47,11 @@ return [
     //
     // For more see: https://github.com/humbug/php-scoper#patchers
     'patchers' => [
-        function (string $filePath, string $prefix, string $content): string {
-            // Change the content here.
+        function (string $filePath, string $prefix, string $contents): string {
+            // Change the contents here.
 
-            return $content;
+            return $contents;
         },
-    ],
-
-    // By default, PHP-Scoper only prefixes code where the namespace is non-global. In other words, non-namespaced
-    // code is not prefixed. This leaves the majority of classes, functions and constants in PHP - and most extensions,
-    // untouched.
-    //
-    // This is not necessarily a desirable outcome for vendor dependencies which are also not namespaced. To ensure
-    // they are isolated, you can configure the following which can be a list of strings or callables taking a string
-    // (the class name) as an argument and return a boolean (true meaning the class is going to prefixed).
-    //
-    // For more, see https://github.com/humbug/php-scoper#global-namespace-whitelisting
-    'global_namespace_whitelist' => [
-        'AppKernel',
     ],
 
     // PHP-Scoper's goal is to make sure that all code for a project lies in a distinct PHP namespace. However, you
@@ -67,6 +64,23 @@ return [
     //
     // Fore more see https://github.com/humbug/php-scoper#whitelist
     'whitelist' => [
-        'PHPUnit\Framework\TestCase',
+        // 'PHPUnit\Framework\TestCase',   // A specific class
+        // 'PHPUnit\Framework\*',          // The whole namespace
+        // '*',                            // Everything
     ],
+
+    // If `true` then the user defined constants belonging to the global namespace will not be prefixed.
+    //
+    // For more see https://github.com/humbug/php-scoper#constants--constants--functions-from-the-global-namespace
+    'whitelist-global-constants' => true,
+
+    // If `true` then the user defined classes belonging to the global namespace will not be prefixed.
+    //
+    // For more see https://github.com/humbug/php-scoper#constants--constants--functions-from-the-global-namespace
+    'whitelist-global-classes' => true,
+
+    // If `true` then the user defined functions belonging to the global namespace will not be prefixed.
+    //
+    // For more see https://github.com/humbug/php-scoper#constants--constants--functions-from-the-global-namespace
+    'whitelist-global-functions' => true,
 ];
